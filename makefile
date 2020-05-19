7eae6cb5454c8bfd6944a7f5823f3a35adcf3d69
Jack Rieck <jack.rieck@sendgrid.com>
2020-05-18 18:34:00 -0600

    build oasis node image

--- makefile
+++ makefile
@@ -1,8 +1,13 @@
-.PHONY: base
+.PHONY: base oasis
 
 # packer builds on Azure require AZ_SUBSCRIPTION_ID, AZ_CLIENT_ID, AZ_CLIENT_SECRET to be set
 
 base:
 	pushd base && \
+	packer build build.json && \
+	popd
+
+oasis:
+	pushd oasis && \
 	packer build build.json && \
 	popd
---
+++ oasis/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -xe
+
+# run utility to check SGX is configured properly before starting
+sgx-detect
+
+# install tools
+sudo apt -y update
+sudo apt -y install -y git wget bubblewrap gcc gcc-c++ \
+protobuf-compiler make cmake openssl-devel libseccomp-devel
+
+# install golang
+wget https://dl.google.com/go/go1.14.3.linux-amd64.tar.gz -O go.tar.gz
+tar -C /usr/local -xzf go.tar.gz
+export PATH=/usr/local/go/bin:$PATH
+
+# clone latest release of oasis-core
+git clone --depth 1 -b v20.6 https://github.com/oasislabs/oasis-core.git
+
+# build oasis
+pushd oasis-core && \
+rustup target add x86_64-fortanix-unknown-sgx && \
+OASIS_UNSAFE_SKIP_AVR_VERIFY=1 OASIS_UNSAFE_SKIP_KM_POLICY=1 make
+popd
