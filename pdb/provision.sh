880af60aeb8f7ed2c96a815324b3c40d7ce26c6c
Jack Rieck <jack.rieck@sendgrid.com>
2020-05-24 16:31:57 -0600

    add provision file

---
+++ pdb/provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+# convert from ELF to sized SGXS format
+# TODO: tune these params
+ftxsgx-elf2sgxs pdb --heap-size 0x20000 --stack-size 0x20000 --threads 10 --debug
+
+# generate signing key
+openssl genrsa -3 3072 > pdb.pem
+
+# sign sgxs file
+# generates pdb.sig, keep this in same dir as sgxs binary
+# TODO: tune these params
+sgxs-sign --key pdb.pem pdb.sgxs pdb.sig -d --xfrm 7/0 --isvprodid 0 --isvsvn 0
