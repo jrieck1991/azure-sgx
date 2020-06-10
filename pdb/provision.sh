2e8021d2fb34976fdb589e5d17fc44f67f15d2d8
jrieck <jack.rieck@sendgrid.com>
2020-06-09 23:25:31 -0400

    Build pdb on build node (#5)
    * Build pdb on build node
    
    * adding build steps
    
    * rm raw convention
    
    * add deps
    
    * update submodule
    
    * new commits
    
    * updated pdb code, successful builds

--- pdb/provision.sh
+++ pdb/provision.sh
@@ -1,13 +1,31 @@
 #!/bin/bash -xe
 
+# workdir
+cd pdb
+
+# install rocksdb
+sudo apt -y update
+sudo apt -y install clang librocksdb-dev
+
+# build binaries
+rustup target add x86_64-fortanix-unknown-sgx && \
+make trusted && make dal
+
+# cp to working dir
+cp target/x86_64-fortanix-unknown-sgx/debug/trusted ../trusted
+cp target/x86_64-unknown-linux-gnu/debug/dal ../dal
+cd ..
+
 # convert from ELF to sized SGXS format
 # TODO: tune these params
-ftxsgx-elf2sgxs pdb --heap-size 0x20000 --stack-size 0x20000 --threads 10 --debug
+ftxsgx-elf2sgxs trusted --heap-size 0x20000 --stack-size 0x20000 --threads 10 --debug
 
 # generate signing key
-openssl genrsa -3 3072 > pdb.pem
+openssl genrsa -3 3072 > trusted.pem
 
 # sign sgxs file
 # generates pdb.sig, keep this in same dir as sgxs binary
 # TODO: tune these params
-sgxs-sign --key pdb.pem pdb.sgxs pdb.sig -d --xfrm 7/0 --isvprodid 0 --isvsvn 0
+sgxs-sign --key trusted.pem trusted.sgxs trusted.sig -d --xfrm 7/0 --isvprodid 0 --isvsvn 0
+
+# to run 'ftxsgx-runner trusted.sgxs'
